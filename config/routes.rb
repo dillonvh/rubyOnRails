a1a53b08e4ba1dc327d721c76cd2058354bd4745
dillon <dillon.j.vh@gmail.com>
2024-10-07 16:28:01 -0700

    Add first name and last name to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+
   resources :posts
+
   get "about", to: "pages#about"
   get "posts", to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
